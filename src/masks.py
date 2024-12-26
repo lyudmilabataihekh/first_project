e1dcf0ef93042ab429a06a6a093cb4c1704ab18b
lyudmila batainekh <blsumbb@gmail.com>
2024-12-27 00:44:27 +0300

    Фиксация первоначального кода

---
+++ src/masks.py
@@ -0,0 +1,21 @@
+def get_mask_card_number(card: str) -> str:
+    """Маскирует номер банковской карты"""
+    if card.isdigit():
+        return f"{card[:4]} {card[4:6]}** **** {card[-4:]}"
+    else:
+        return f"{card[:-12]} {card[-12:-10]}** **** {card[-4:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Маскирует номер банковского счета"""
+    if account_number.isdigit():
+        return f"**{account_number[-4:]}"
+    elif "Счет" in account_number:
+        return f"Счет **{account_number[-4:]}"
+
+
+if __name__ == '__main__':
+    input_card_number = input("Введите карту: ")
+    print(get_mask_card_number(input_card_number))
+    input_account_number = input("Введите счет: ")
+    print(get_mask_account(input_account_number))
