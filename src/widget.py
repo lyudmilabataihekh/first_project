9afb9fcb932d349ddcc3268509e0a39604c537b7
lyudmila batainekh <blsumbb@gmail.com>
2024-12-27 01:32:38 +0300

    Добавление функции, которая меняет формат даты

--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,5 @@
 from src.masks import get_mask_account, get_mask_card_number
+import re
 
 
 def mask_account_card(account_card_data: str) -> str:
@@ -11,6 +12,20 @@ def mask_account_card(account_card_data: str) -> str:
         return card_data
 
 
+def get_date(date: str) -> str:
+    """Меняет формат даты"""
+    formated_date = re.sub(r"[/.-]", "", date)
+
+    year = formated_date[:4]
+    month = formated_date[4:6]
+    day = formated_date[6:8]
+
+    modified_format = f"{day}.{month}.{year}"
+    return modified_format
+
+
 if __name__ == '__main__':
     account_card_data_input = input("Введите тип и номер карты или счета: ")
     print(mask_account_card(account_card_data_input))
+    date_input = input("Введите дату: ")
+    print(get_date(date_input))
