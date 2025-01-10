1f2471f22cbcee4b14e8671327238456a9760baf
lyudmila batainekh <blsumbb@gmail.com>
2025-01-10 23:03:48 +0300

    Добавление функции, которая сортирует данные в соответствии с указанным статусом

---
+++ src/processing.py
@@ -0,0 +1,29 @@
+from typing import List, Dict
+
+
+data = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+
+def filter_by_state(data: List[Dict[str, str]], state: str ='EXECUTED') -> List[Dict[str, str]]:
+    """Принимает список словарей, опциональное значение для ключа state
+        и возвращает новый список с указанным значением"""
+    filtered_items = []
+    for item in data:
+        if item.get('state') == state:
+            filtered_items.append(item)
+    return filtered_items
+
+
+if __name__ == "__main__":
+    # Выход функции со статусом по умолчанию 'EXECUTED'
+    executed_items = filter_by_state(data)
+    print(executed_items)
+
+    # Выход функции, если вторым аргументом передано 'CANCELED'
+    canceled_items = filter_by_state(data, state="CANCELED")
+    print(canceled_items)
