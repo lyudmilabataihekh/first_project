5a4b54c0ea36f231ded9299ab64366e387c7d343
lyudmila batainekh <blsumbb@gmail.com>
2025-01-21 05:17:10 +0300

    обавление тестов в новый модуль test_widget и доработка функций в модулях widget и masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.masks import get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 @pytest.mark.parametrize(
@@ -25,3 +25,29 @@ from src.masks import get_mask_card_number
 )
 def test_get_mask_card_number(input_card: str, expected_output: str) -> None:
     assert get_mask_card_number(input_card) == expected_output
+
+
+@pytest.mark.parametrize(
+    "input_account_number, expected_output",
+    [
+        # Различные входные форматы
+        ("1234567890123456", "**3456"),
+        ("123 456 789 0123456", "**3456"),
+        ("123-456-789-0123456", "**3456"),
+        ("123.456.789.0123456", "**3456"),
+        # Разные длины
+        ("1234567890", ""),
+        ("12345678", ""),
+        ("1", ""),
+        ("", ""),
+        # Неверные форматы
+        ("abc", ""),  # Буквы
+        ("1234 5678 9012 abcd", ""),
+        ("12345678901234567", ""),
+        ("12a3456789", ""),
+        ("12345678*", ""),
+        ("1234567890.1234", ""),
+    ],
+)
+def test_get_mask_account(input_account_number: str, expected_output: str) -> None:
+    assert get_mask_account(input_account_number) == expected_output
