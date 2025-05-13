ce6d045beebaccecf9c1523940eeb189624f132b
lyudmila batainekh <blsumbb@gmail.com>
2025-05-14 01:27:52 +0300

    Добавление логов в модули masks и utils

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,20 @@
+import logging
 import re
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename='logs/masks.log',
+                    filemode='w')
+
+logger = logging.getLogger('masks')
 
 
 def get_mask_card_number(card: str) -> str:
     """Маскирует номер банковской карты"""
+    logger.info("Обработка номера карты: %s", card)
     card = re.sub(r"[-.\/ ]", "", card)
     if len(card) != 16 or not card.isdigit():
+        logger.warning("Некорректный номер карты: %s", card)
         return ""
     else:
         return f"{card[:4]} {card[4:6]}** **** {card[-4:]}"
@@ -12,10 +22,12 @@ def get_mask_card_number(card: str) -> str:
 
 def get_mask_account(account_number: str) -> str:
     """Маскирует номер банковского счета"""
+    logger.info("Обработка банковского счета: %s", account_number)
     account_number = re.sub(r"[-.\/ ]", "", account_number)
     if "Счет" in account_number:
         return f"Счет **{account_number[-4:]}"
     elif len(account_number) != 16 or not account_number.isdigit():
+        logger.warning("Некорректный номер счета: %s", account_number)
         return ""
     else:
         return f"**{account_number[-4:]}"
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,13 @@
 import json
+import logging
 from typing import Any, List
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    filename='logs/utils.log',
+                    filemode='w')
+
+logger = logging.getLogger('utils')
 
 
 def get_financial_operation(file_path: str) -> List[Any]:
@@ -7,12 +15,16 @@ def get_financial_operation(file_path: str) -> List[Any]:
     try:
         with open(file_path, "r", encoding="utf-8") as json_file:
             result = json.load(json_file)
+            logger.info("Загрузка данных в файл: %s", file_path)
             if not result:
+                logger.warning("Загрузка данных не удалась")
                 return []
             return result
-    except json.JSONDecodeError:
+    except json.JSONDecodeError as ex:
+        logger.error(f"Произошла ошибка: {ex}")
         return []
-    except FileNotFoundError:
+    except FileNotFoundError as ex:
+        logging.error(f"Произошла ошибка: {ex}")
         return []
 
 
